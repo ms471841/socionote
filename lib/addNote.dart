5f4c0362fddae285ba772aa4116e5274e16636ad
ms471841 <ms471841@.com>
2021-05-10 18:47:29 +0530

    changes done

--- lib/addNote.dart
+++ lib/addNote.dart
@@ -8,10 +8,10 @@ class AddNote extends StatefulWidget {
 }
 
 class _AddNoteState extends State<AddNote> {
-
-
-  
-
+  TimeOfDay timeOfDay = TimeOfDay.now();
+  DateTime dateTime = DateTime.now();
+  TextEditingController titlecontroller;
+  TextEditingController bodycontroller;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,15 +23,45 @@ class _AddNoteState extends State<AddNote> {
         actions: [
           IconButton(
             icon: Icon(Icons.arrow_right),
-            onPressed: () {},
+            onPressed: () {
+              print(titlecontroller.text);
+            },
           ),
         ],
       ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(16),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(TimeOfDay.now().toString()),
+            Text(
+              timeOfDay.format(context),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: titlecontroller,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+              decoration: InputDecoration(
+                hintText: "Title",
+                enabledBorder: InputBorder.none,
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: bodycontroller,
+              keyboardType: TextInputType.multiline,
+              style: TextStyle(
+                fontSize: 18,
+              ),
+              decoration: InputDecoration(
+                hintText: "Type something....",
+                enabledBorder: InputBorder.none,
+              ),
+            ),
           ],
         ),
       ),
